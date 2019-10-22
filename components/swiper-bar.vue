34c05285b225e52bd13182bde504e9280f7ce5b6
Edmund-Wong <1592193221@qq.com>
2019-10-22 11:13:49 +0800

    tab卡组件

--- components/swiper-bar.vue
+++ components/swiper-bar.vue
@@ -1,31 +1,44 @@
 <template>
-    <scroll-view class="container">
-       <scroll-view scroll-x="true"
-                    scroll-y="true"
-                    @scrolltoupper="upper" 
-                    @scrolltolower="lower"
-                    @scroll="scroll">
-                    <view id="demo1" class="scroll-view-item uni-bg-red">A</view>
-                    <view id="demo2" class="scroll-view-item uni-bg-green">B</view>
-                    <view id="demo3" class="scroll-view-item uni-bg-blue">C</view>
-                </scroll-view>
+    <view class="container">
+        <scroll-view scroll-x="true"
+                    scroll-y="true">
+                    <view   class="scroll-view-item"
+                            v-for="(item,idx) of 20"
+                            scroll-top="200"
+                            :style="[defaultStyle]"
+                            :key="idx">
+                      NBA大老粗
+                    </view>
+                    
+        </scroll-view>
+    </view>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
-  },
-  methods: {
-    scroll() {
-      console.log('wongwongwong')
+    return {
+      defaultStyle: {
+        color: 'red',
+        fontSize: 24 + 'rpx'
+      }
     }
-  }
+  },
+  methods: {}
 }
 </script>
 
 <style lang="scss" scoped>
 scroll-view {
-  height: 100rpx;
+  width: 100vw;
+  display: flex;
+  white-space: nowrap;
+  align-items: center;
+  .scroll-view-item {
+    display: inline-block;
+    margin: 0 58rpx;
+    line-height: 76rpx;
+    font-size: 36rpx;
+  }
 }
 </style>
