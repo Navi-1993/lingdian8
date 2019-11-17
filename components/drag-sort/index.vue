870e8d7088f40b437181120be53be66d11be7bfd
Edmund <1592193221@qq.com>
2019-11-17 21:36:55 +0800

    feature 修改源码 优化更改数据的逻辑

--- components/drag-sort/index.vue
+++ components/drag-sort/index.vue
@@ -3,7 +3,7 @@
  * @Author: Edmund
  * @Email: q1592193221@gmail.com
  * @Date: 2019-11-16 18:28:05
- * @LastEditTime: 2019-11-16 22:58:13
+ * @LastEditTime: 2019-11-17 21:33:49
  * @LastEditors: Edmund
  -->
 <template>
@@ -24,7 +24,7 @@
         style="height:55px"
         :class="{ active: active === index, 'vh-1px-t': item.index > 0 }"
       >
-        <view class="item" @tap="deleteItem">{{ item[props.label] }}</view>
+        <view class="item">{{ item[props.label] }}</view>
         <view class="touch-tight">
           <view class="ico_drag"></view>
         </view>
@@ -45,224 +45,214 @@
 
 <script>
 export default {
-  name: 'drag-sort',
-  mixins: [],
-  components: {},
-  data() {
-    return {
-      height: 55, // 高度
-      currentList: [],
-      active: -1, // 当前激活的item
-      itemIndex: 0, // 当前激活的item的原index
-      topY: 0, // 距离顶部的距离
-      deviationY: 0 // 偏移量
-    }
-  },
-  computed: {
-    // 动态计算拖拽组件高度
-    currentListLength() {
-      return this.currentList.length * this.height
-    }
-  },
-  props: {
-    list: {
-      type: Array,
-      default: () => {
-        return []
-      }
-    },
-    props: {
-      type: Object,
-      default: () => {
-        return {
-          label: 'label',
-          value: 'value'
-        }
-      }
-    }
-  },
-  watch: {
-    // 对父组件来值list进行监听，当发生变化时，执行更新方法
-    list(val) {
-      this.onUpdateCurrentList()
-    }
-  },
-  created() {
-    this.onUpdateCurrentList()
-  },
-  mounted() {},
-  updated() {},
-  filters: {},
-  methods: {
-    deleteItem() {
-      this.currentList.push({ key: `${new Date() * 1}` })
-      this.$forceUpdate()
-    },
-    /**
-     * @Description: 更新数据
-     * @param {type}
-     * @return:
-     */
-    onUpdateCurrentList() {
-      let arr = []
-      for (let key in this.list) {
-        arr.push({
-          ...this.list[key],
-          index: Number(key),
-          y: key * this.height,
-          animation: true
-        })
-      }
-      this.currentList = arr
-    },
-    touchstart(e) {
-      // 计算y轴点击位置
-      var query = wx.createSelectorQuery().in(this)
-      query.select('#drag').boundingClientRect()
-      query.exec(res => {
-        console.log('点击元素信息:', res)
-        this.topY = res[0].top
-        let touchY = e.mp.touches[0].clientY - res[0].top
-        this.deviationY = touchY % this.height
-        // console.log(touchY)
-        // 点击时对每个数组元素进行判断
-        for (let key in this.currentList) {
-          if (
-            this.currentList[key].index * this.height <= touchY &&
-            (this.currentList[key].index + 1) * this.height >= touchY
-          ) {
-            console.log('seleted success')
-            this.active = Number(key)
-            this.itemIndex = this.currentList[key].index
-            break
-          }
-        }
-      })
-    },
-    touchmove(e) {
-      // 没有选中元素则退出
-      if (this.active < 0) return
-      let touchY = e.mp.touches[0].clientY - this.topY - this.deviationY
-      console.log('鼠标当前y坐标', touchY)
-      // 更新激活item的y坐标
-      this.currentList[this.active].y = touchY
-      for (const key in this.currentList) {
-        // 跳过当前操作的item
-        if (
-          this.currentList[key].index !== this.currentList[this.active].index
-        ) {
-          if (
-            this.currentList[key].index > this.currentList[this.active].index
-          ) {
-            if (
-              touchY >
-              this.currentList[key].index * this.height - this.height / 2
-            ) {
-              this.currentList[this.active].index = this.currentList[key].index
-              this.currentList[key].index = this.currentList[key].index - 1
-              this.currentList[key].y =
-                this.currentList[key].index * this.height
-              break
-            }
-          } else {
-            // 若移动距离超过格子的一半，则更新所有元素的索引值
-            if (
-              touchY <
-              this.currentList[key].index * this.height + this.height / 2
-            ) {
-              console.log('activeItem idx', this.currentList[this.active].index)
-              this.currentList[this.active].index = this.currentList[key].index
-              this.currentList[key].index = this.currentList[key].index + 1
-              this.currentList[key].y =
-                this.currentList[key].index * this.height
-              break
-            }
-          }
-        }
-      }
-    },
-    touchend(e) {
-      if (
-        this.itemIndex !== this.currentList[this.active].index &&
-        this.active > -1
-      ) {
-        this.$emit('change', {
-          // 活动中的item
-          item: (() => {
-            let data = {
-              ...this.currentList[this.active]
-            }
-            delete data.index
-            delete data.y
-            delete data.animation
-            return data
-          })(),
-          // 活动后item前面的元素
-          frontItem: (() => {
-            for (const iterator of this.currentList) {
-              if (this.currentList[this.active].index - 1 === iterator.index) {
-                let data = {
-                  ...iterator
-                }
-                delete data.index
-                delete data.y
-                delete data.animation
-                return data
-              }
-            }
-          })()
-        })
-      }
-      console.log('改变前item的y=', this.currentList[this.active].y)
-      this.currentList[this.active].animation = true
-      this.currentList[this.active].y =
-        this.currentList[this.active].index * this.height
-      console.log('改变后item的y=', this.currentList[this.active].y)
-      this.active = -1
-      // 修正拖拽后出错的高度
-    }
-  }
+	name: 'drag-sort',
+	mixins: [],
+	components: {},
+	data() {
+		return {
+			height: 55, // 高度
+			currentList: [],
+			active: -1, // 当前激活的item
+			itemIndex: 0, // 当前激活的item的原index
+			topY: 0, // 距离顶部的距离
+			deviationY: 0, // 偏移量
+			oldIdx: 0
+		}
+	},
+	computed: {
+		// 动态计算拖拽组件高度
+		currentListLength() {
+			return this.currentList.length * this.height
+		}
+	},
+	props: {
+		list: {
+			type: Array,
+			default: () => {
+				return []
+			}
+		},
+		props: {
+			type: Object,
+			default: () => {
+				return {
+					label: 'label',
+					value: 'value'
+				}
+			}
+		}
+	},
+	watch: {
+		// 对父组件来值list进行监听，当发生变化时，执行更新方法
+		list(val) {
+			this.onUpdateCurrentList()
+			this.$foreceUpdate()
+		}
+	},
+	created() {
+		this.onUpdateCurrentList()
+	},
+	mounted() {},
+	updated() {},
+	filters: {},
+	methods: {
+		// deleteItem() {
+		// 	this.currentList.push({ key: `${new Date() * 1}` })
+		// 	this.$forceUpdate()
+		// },
+		/**
+		 * @Description: 更新数据
+		 * @param {type}
+		 * @return:
+		 */
+		onUpdateCurrentList() {
+			let arr = []
+			for (let key in this.list) {
+				arr.push({
+					...this.list[key],
+					index: Number(key),
+					y: key * this.height,
+					animation: true
+				})
+			}
+			this.currentList = arr
+		},
+		touchstart(e) {
+			// 1. 利用微信API检测元素信息
+			const query = wx.createSelectorQuery().in(this)
+			query.select('#drag').boundingClientRect()
+			// 2. 于回调中返回查询结果
+			console.log('点击元素信息', e.mp.touches[0].clientY)
+			query.exec((res) => {
+				console.log('查询节点信息:', res[0].top)
+				this.topY = res[0].top // 节点的上边界坐标
+				let touchY = e.mp.touches[0].clientY - res[0].top
+
+				// 计算出点击时y轴的偏移量
+				this.deviationY = touchY % this.height
+				console.log(this.deviationY)
+				// 点击时对每个数组元素进行判断
+				for (let key in this.currentList) {
+					if (
+						this.currentList[key].index * this.height <= touchY &&
+						(this.currentList[key].index + 1) * this.height >= touchY
+					) {
+						this.active = key * 1
+						this.oldIdx = key * 1
+						this.itemIndex = this.currentList[key].index
+						break
+					}
+				}
+			})
+		},
+		touchmove(e) {
+			// 没有选中元素则退出
+			if (this.active < 0) return
+			let touchY = e.mp.touches[0].clientY - this.topY - this.deviationY
+			console.log('鼠标当前y坐标', touchY)
+			// 更新激活item的y坐标
+			this.currentList[this.active].y = touchY
+			for (const key in this.currentList) {
+				// 跳过当前操作的item
+				if (
+					this.currentList[key].index !== this.currentList[this.active].index
+				) {
+					if (
+						this.currentList[key].index > this.currentList[this.active].index
+					) {
+						if (
+							touchY >
+							this.currentList[key].index * this.height - this.height / 2
+						) {
+							this.currentList[this.active].index = this.currentList[key].index
+							this.currentList[key].index = this.currentList[key].index - 1
+							this.currentList[key].y =
+								this.currentList[key].index * this.height
+							break
+						}
+					} else {
+						// 若移动距离超过格子的一半，则更新所有元素的索引值
+						if (
+							touchY <
+							this.currentList[key].index * this.height + this.height / 2
+						) {
+							console.log('activeItem idx', this.currentList[this.active].index)
+							this.currentList[this.active].index = this.currentList[key].index
+							this.currentList[key].index = this.currentList[key].index + 1
+							this.currentList[key].y =
+								this.currentList[key].index * this.height
+							break
+						}
+					}
+				}
+			}
+		},
+		touchend(e) {
+			if (
+				this.itemIndex !== this.currentList[this.active].index &&
+				this.active > -1
+			) {
+				this.$emit('change', {
+					// 活动后的item
+					item: (() => {
+						let data = {
+							...this.currentList[this.active],
+							oldIdx: this.oldIdx
+						}
+						return data
+					})()
+				})
+			}
+			// console.log('改变前item的y=', this.currentList[this.active].y)
+			this.currentList[this.active].animation = true
+			this.currentList[this.active].y =
+				this.currentList[this.active].index * this.height
+			// console.log('改变后item的y=', this.currentList[this.active].y)
+			// 退出拖拽事件后让active变为无选取状态
+			this.active = -1
+		}
+	}
 }
 </script>
 
 <style lang="scss" scoped>
 @import '~./1px.scss';
 .drag-sort {
-  width: 100%;
+	width: 100%;
 }
 .drag-sort-item {
-  position: absolute !important;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  padding: 0;
-  margin: 0;
-  background: #fff;
-  padding: 0 15rpx;
-  box-sizing: border-box;
-  .item {
-    flex: 1;
-  }
-  .touch-tight {
-    width: 24rpx;
-    display: flex;
-    justify-content: center;
-  }
+	position: absolute !important;
+	display: flex;
+	align-items: center;
+	width: 100%;
+	padding: 0;
+	margin: 0;
+	background: #fff;
+	padding: 0 15rpx;
+	box-sizing: border-box;
+	.item {
+		flex: 1;
+	}
+	.touch-tight {
+		width: 24rpx;
+		display: flex;
+		justify-content: center;
+	}
 }
 .touch {
-  height: 100%;
-  width: 50rpx;
+	height: 100%;
+	width: 50rpx;
 }
 .ico_drag {
-  display: inline-block;
-  width: 24rpx;
-  height: 12px;
-  background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAYCAYAAAC8/X7cAAAAAXNSR0IArs4c6QAAAEtJREFUWAnt1cEJACAMA0B1/506moIr5FEK51+Jl0d2Vd01+JzB2X90H5jeoPwECBDIBLYlzgDj25Y4JvQAAQIERgtY4u76LHF3Aw8rGQnK3sYAXQAAAABJRU5ErkJggg==)
-    0 0 no-repeat;
-  background-size: 100% auto;
+	display: inline-block;
+	width: 24rpx;
+	height: 12px;
+	background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAYCAYAAAC8/X7cAAAAAXNSR0IArs4c6QAAAEtJREFUWAnt1cEJACAMA0B1/506moIr5FEK51+Jl0d2Vd01+JzB2X90H5jeoPwECBDIBLYlzgDj25Y4JvQAAQIERgtY4u76LHF3Aw8rGQnK3sYAXQAAAABJRU5ErkJggg==)
+		0 0 no-repeat;
+	background-size: 100% auto;
 }
 .active {
-  box-shadow: 0 0 40rpx #dddddd;
-  z-index: 99;
+	box-shadow: 0 0 40rpx #dddddd;
+	z-index: 99;
 }
 </style>
--- pages/user/drag.vue
+++ pages/user/drag.vue
@@ -3,7 +3,7 @@
  * @Author: Edmund
  * @Email: q1592193221@gmail.com
  * @Date: 2019-11-06 17:16:48
- * @LastEditTime: 2019-11-17 00:12:43
+ * @LastEditTime: 2019-11-17 21:36:02
  * @LastEditors: Edmund
  -->
 
@@ -19,109 +19,102 @@
 let that
 import dragSort from 'components/drag-sort/index.vue'
 export default {
-  name: 'drag',
-  components: { dragSort },
-  props: {},
-  data() {
-    return {
-      props: {
-        label: 'key'
-      },
-      list: [
-        {
-          key: '标题1'
-        },
-        {
-          key: '标题2'
-        },
-        {
-          key: '标题3'
-        },
-        {
-          key: '标题4'
-        },
-        {
-          key: '标题5'
-        }
-      ]
-    }
-  },
-  beforeCreate() {
-    // #ifndef APP-PLUS
-    console.time('renderTime') // 创建到渲染所耗时间，大于3秒则加入优化计划
-    // #endif
-  },
-  created() {
-    that = this
-    // 测试时使用
-    uni.getSystemInfo({
-      success: res => {
-        console.log('手机品牌', res.brand) // 手机品牌
-        console.log('手机型号', res.model) // 手机型号
-        console.log('可使用窗口的顶部位置', res.windowTop)
-        this.windowHeight = res.windowHeight
-        console.log('状态栏的高度', res.statusBarHeight)
-        console.log('可使用的窗口高度 = 100vh', res.windowHeight)
-        console.log('可使用窗口的底部位置', res.windowBottom)
-        console.log('导航栏的高度	', res.navigationBarHeight)
-      }
-    })
-  },
-  beforeMount() {},
-  mounted() {},
-  onLoad() {},
-  onShow() {},
-  onReady() {
-    // #ifndef APP-PLUS
-    console.timeEnd('renderTime')
-    console.log(`%c${that.__route__}`, 'color:yellow;background-color:black')
-    // #endif
-  },
-  onHide() {},
-  onUnload() {},
-  beforeDestroy() {},
-  destroyed() {},
-  methods: {
-    /**
-     * @Description: drag-组件$emit change 时更改 Item
-     * @e {Object} drag-组件定义的事件值
-     * @return: void
-     */
-    itemChange(e) {
-      let currentItem = e.item
-      let frontItem = e.frontItem
+	name: 'drag',
+	components: { dragSort },
+	props: {},
+	data() {
+		return {
+			props: {
+				label: 'key'
+			},
+			list: [
+				{
+					key: '标题1'
+				},
+				{
+					key: '标题2'
+				},
+				{
+					key: '标题3'
+				},
+				{
+					key: '标题4'
+				},
+				{
+					key: '标题5'
+				}
+			]
+		}
+	},
+	beforeCreate() {
+		// #ifndef APP-PLUS
+		console.time('renderTime') // 创建到渲染所耗时间，大于3秒则加入优化计划
+		// #endif
+	},
+	created() {
+		that = this
+		// 测试时使用
+		uni.getSystemInfo({
+			success: (res) => {
+				console.log('手机品牌', res.brand) // 手机品牌
+				console.log('手机型号', res.model) // 手机型号
+				console.log('可使用窗口的顶部位置', res.windowTop)
+				this.windowHeight = res.windowHeight
+				console.log('状态栏的高度', res.statusBarHeight)
+				console.log('可使用的窗口高度 = 100vh', res.windowHeight)
+				console.log('可使用窗口的底部位置', res.windowBottom)
+				console.log('导航栏的高度	', res.navigationBarHeight)
+			}
+		})
+	},
+	beforeMount() {},
+	mounted() {},
+	onLoad() {},
+	onShow() {},
+	onReady() {
+		// #ifndef APP-PLUS
+		console.timeEnd('renderTime')
+		console.log(`%c${that.__route__}`, 'color:yellow;background-color:black')
+		// #endif
+	},
+	onHide() {},
+	onUnload() {},
+	beforeDestroy() {},
+	destroyed() {},
+	methods: {
+		/**
+		 * @Description: drag-sort组件$emit change 时更改 Item
+		 * @e {Object} drag-组件定义的事件值
+		 * @return: void
+		 */
+		itemChange(e) {
+			let currentItem = e.item
 
-      // 刷新数据
-      that.updateList(currentItem, frontItem)
-    },
-    updateList(currentItem, frontItem) {
-      console.log('currentItem', currentItem)
-      console.log('frontItem', frontItem)
-      // TODO:从数组中找1个符合需求的元素，返回其索引
-      let idx = this.$_.findIndex(this.list, item => {
-        return item.key === currentItem.key
-      })
-      console.log('result', idx)
-    }
-  },
-  computed: {},
-  watch: {}
+			// 刷新数据
+			that.updateList(currentItem)
+		},
+		updateList(currentItem) {
+			console.log('currentItem', currentItem)
+			// TODO:从数组中找1个符合需求的元素，返回其索引
+			let currentIdx = currentItem.index
+			let frontIdx = currentItem.oldIdx
+			// 交换数组元素位置
+			let temp = this.list[currentIdx]
+			this.list[currentIdx] = this.list[frontIdx]
+			this.list[frontIdx] = temp
+			console.log('result', currentIdx)
+			console.log('front', frontIdx)
+			console.log('list', this.list)
+		}
+	},
+	computed: {},
+	watch: {}
 }
 </script>
 
 <style lang="scss" scoped>
 .container {
-  height: 100vh;
-  overflow: auto;
-  .moveBox {
-    height: 600rpx;
-    width: 100vw;
-    background-color: gray;
-    .moveItem {
-      height: 80rpx;
-      width: 100vw;
-      background: red;
-    }
-  }
+	height: 100vh;
+	overflow: auto;
 }
 </style>
